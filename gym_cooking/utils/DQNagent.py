0b157d9614d5c7cfb313a303edb847b04f43d189
Chin Wan <k21016106@kcl.ac.uk>
2024-04-02 22:33:06 +0200

    Completed comments

--- gym_cooking/utils/DQNagent.py
+++ gym_cooking/utils/DQNagent.py
@@ -4,6 +4,10 @@ import numpy as np
 from utils.dlmodel import *
 
 class DQNAgent:
+    """
+    The agent that holds the information and models for deep learning update, 
+    while executing actions for updates.
+    """
     def __init__(self, arglist, st_size, action_size, name, color, role, agent_index, dlmodel_name, gamma=0.95, epsilon=0.8):
         self.name = name
         self.st_size = st_size
@@ -25,13 +29,21 @@ class DQNAgent:
         self.dlmodel = DLModel(self.st_size, self.action_size, self.dlmodel_name, arglist)
         self.memory = UER_memory(self.maxCapacity)
 
+        # Actions that are legal
         self.plausibleActions = []
 
         self.maxEpsilon = 0.8
         self.minEpsilon = 0.02
         self.maxExplorationSteps = 20000000
     
-    def legal_actions(self, legalActions):
+    def legal_actons(self, legalActions):
+        """
+        Add actions that are legal in the environment, 
+        utilizing for the epsilon-greedy algorithm.
+
+        Args:
+            legalActions: The actions that are legal in the environment
+        """
         self.plausibleActions.clear()
         actions_available = [(-1, 0), (1, 0), (0, -1), (0, 1)]
         for action in legalActions:
@@ -40,8 +52,13 @@ class DQNAgent:
 
     def epsilon_greedy(self, state):
         """
-        Epsilon-greedy on action selection
-        Need to implement legal action part 
+        Epsilon-greedy on action selection, allow exploration-exploitation
+        trade-off.
+
+        Args:
+            state: The current state of the environment
+        Return:
+            The maximum action number for update
         """
         value = random.randint(0, 1)
         if value < self.epsilon:
@@ -50,15 +67,35 @@ class DQNAgent:
             return self.dlmodel.max_Q_action(state, self.plausibleActions)
 
     def epsilon_decay(self):
+        """
+        The epsilon-decay function to gradually reduce epsilon through time, 
+        reducing exploration possibilities.
+        """
         self.current += 1
         if self.current < self.maxExplorationSteps:
             self.epsilon = (self.maxEpsilon - self.minEpsilon) * ((self.maxExplorationSteps - self.current)/self.maxExplorationSteps) + self.minEpsilon
     
     def update_target(self):
+        """
+        To update the target when frequency matches
+        """
         if self.current % self.frequency == 0:
             self.dlmodel.update_target()
+    
+    def update_result(self):
+        """
+        To train the model based on the update of DQN algorithm.
+        """
+        batch_used = self.memory.uniform_sample(self.batchSize)
+        x, y, errors = self.y_i_update(batch_used)
+        self.dlmodel.train_model(x, y)
 
     def y_i_update(self, batch_used):
+        """
+        The main update algorithm for training, update 
+        the y value based on the batch sampled uniformly from Uniform Experience
+        Memory file. 
+        """
         current_states = np.array([batch[0] for batch in batch_used])
         next_states = np.array([batch[3] for batch in batch_used])
         predict_current = self.dlmodel.predict(current_states)
@@ -71,34 +108,55 @@ class DQNAgent:
         for i in range(0, len(batch_used)):
             cState, actionSelected, reward, done = batch_used[i][0], batch_used[i][1][self.name], batch_used[i][2], batch_used[i][4]
 
-            t = predict_current[i]
-            oldVal = t[actionSelected]
-            if done: t[actionSelected] = reward
-            else: t[actionSelected] = reward + self.gamma * np.max(predict_next_target[i])
+            batchSelected = predict_current[i]
+            oldVal = batchSelected[actionSelected]
+
+            # Check if it is in the last state
+            if done: batchSelected[actionSelected] = reward
+            else: batchSelected[actionSelected] = reward + self.gamma * np.max(predict_next_target[i])
         
             x[i] = cState
-            y[i] = t
-            errors[i] = np.abs(t[actionSelected] - oldVal)
+            y[i] = batchSelected
+            errors[i] = np.abs(batchSelected[actionSelected] - oldVal)
 
         return x, y, errors
 
-    def update_result(self):
-        batch_used = self.memory.uniform_sample(self.batchSize)
-        x, y, errors = self.y_i_update(batch_used)
-        self.dlmodel.train_model(x, y)
+    def load_model_trained(self):
+        """
+        Load the model weights that are trained for
+        testing purposes.
+        """
+        self.dlmodel.non_test_weight_loading()
     
     def set_alpha_and_epsilon(self):
+        """
+        Setting alpha and epsilon to 0 for testing environment,
+        currently archived.
+        """
         self.alpha = 0
         self.epsilon = 0
     
     def predict(self, state):
+        """
+        The predict function during testing, return maximum actions
+        without possibility of explorations.
+        
+        Args:
+            state: Current state of the environment
+        Return:
+            The maximum action number
+        """
         return self.dlmodel.max_Q_action(state, self.plausibleActions)
 
     def observeTransition(self, transition):
+        """
+        Store the transition of the current step to the memory.
+
+        Args;
+            transition: The values that should be stored
+        """
         self.memory.store_transition(transition)
     
-    def load_model_trained(self):
-        self.dlmodel.non_test_weight_loading()
 
     
     
--- gym_cooking/utils/dlmodel.py
+++ gym_cooking/utils/dlmodel.py
@@ -5,52 +5,92 @@ from tensorflow.python.keras.optimizers import *
 import numpy as np
 
 class DLModel:
+    """
+    The main model for deep learning simulation and updates.
+    """
     def __init__(self, state_sizes, action_sizes, name, arglist):
         self.state_sizes = state_sizes
         self.action_sizes = action_sizes
         self.name = name
-        self.alpha = arglist.learning_rate
-        self.num_nodes = arglist.num_nodes
         self.model = self.build_and_compile_model()
         self.targetModel = self.build_and_compile_model()
+        self.alpha = arglist.learning_rate
+        self.num_nodes = arglist.num_nodes
         self.actions = [(-1, 0), (1, 0), (0, -1), (0, 1)]
     
     def load_model_trained(self):
+        """
+        Load the model to be trained, currently archived.
+        """
         self.model = load_model(self.name)
     
-    def update_target(self):
-        self.targetModel.set_weights(self.model.get_weights())
-    
     def build_and_compile_model(self):
+        """
+        Build the models required for the deep Q-learning.
+        Return:
+            The model created and compiled
+        """
         x = Input(shape=(self.state_sizes,))
         x1 = Dense(self.num_nodes, activation='relu')(x)
         x2 = Dense(self.num_nodes, activation='relu')(x1)    
         z =  Dense(self.action_sizes, activation='softmax')(x2)
         model = Model(inputs=x, outputs=z)
-
         model.compile(loss="MeanSquaredError", optimizer="adam")
         
         return model
     
-    def non_test_weight_loading(self):
-        self.model.load_weights(self.name)
+    def update_target(self):
+        """
+        Update the target model weights for later training.
+        """
+        self.targetModel.set_weights(self.model.get_weights())
     
-    def train_model(self, X, y, epochs=10, verbose=0):
-        self.model.fit(X, y, batch_size=len(X), epochs=epochs, verbose=verbose)
-
     def predict(self, state, target=False):
+        """
+        Predict the probabilities based on the state.
+
+        Args:
+            state: The representation of the environment
+            target: Whether to update with target, used in y_update
+        Return:
+            The predict result
+        """
         if not target:
             return self.model.predict(state)
         else:
             return self.targetModel.predict(state)
+
+    def non_test_weight_loading(self):
+        """
+        To load the weights needed for final testing.
+        """
+        self.model.load_weights(self.name)
     
+    def train_model(self, X, y, epochs=10, verbose=0):
+        """
+        Fit the model based on epochs and verbose requirements,
+        used during the y_update.
+
+        Args:
+            X: The values for training
+            y: The "correct" values in the reinforcement learning framework
+            epochs: Number of times the training continues
+            verbose: The logging level
+        """
+        self.model.fit(X, y, batch_size=len(X), epochs=epochs, verbose=verbose)
+
     def max_Q_action(self, state, legalActions, target=False):
+        """
+        Return the action that has the maximum Q-value, used during epsilon-greedy
+        algorithm. As not all actions are legal, eliminations are required.
+        """
         actions = self.predict(state.reshape(1, self.state_sizes), target=target)
 
         finalList = actions.flatten()
         maxIndex = 1000
         maxValue = float("-inf")
 
+        # Remove actions that are not legal
         for i in range(len(finalList)):
             if i not in legalActions:
                 continue
@@ -60,4 +100,7 @@ class DLModel:
         return maxIndex
 
     def save_model(self):
+        """
+        Save the model to the filename
+        """
         self.model.save(self.name)
